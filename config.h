67170aa3cd06919eb5f55a923b4e3a23658d1383
Xiaowei.Chai <root@localhost.localdomain>
2015-12-22 11:21:54 -0500

    modify soft connect file

---
+++ config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "streamserver"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "xiaowei.chai@allegion.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "StreamServer"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "StreamServer 0.0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "streamserver"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.0.1"
+
+/* Version number of package */
+#define VERSION "0.0.1"

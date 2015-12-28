1318bf1a44a98437fe84e8c6cd8409e920568c20
work pc <xiaowei.chai@allegion.com>
2015-12-28 06:38:49 -0500

    use eXosip

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <eXosip2/eXosip.h>
 using namespace std;
 int main(int argc, char const *argv[])
 {
+	struct eXosip_t *context_eXosip;
 	cout<<"Hello Stream Server"<<endl;
+	int ret  = eXosip_init(context_eXosip);
+	if(ret!=0)
+	{
+		cout<<"inti eXosip error"<<endl;
+	}
 	return 0;
 }

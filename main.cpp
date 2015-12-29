e04a028431ba3f8dd3b80a46ee1324a8fa94cc33
work pc <xiaowei.chai@allegion.com>
2015-12-28 22:22:29 -0500

    can running

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <glog/logging.h>
 #include <eXosip2/eXosip.h>
 #include <time.h>
 using namespace std;
 int main(int argc, char const *argv[])
 {
-	struct eXosip_t *context_eXosip;
+	
 	cout<<"Hello Stream Server"<<endl;
+	FLAGS_log_dir="./";
+	google::InitGoogleLogging(argv[0]);
+	struct eXosip_t  *context_eXosip=eXosip_malloc();
 	int ret  = eXosip_init(context_eXosip);
 	if(ret!=0)
 	{
-		cout<<"inti eXosip error"<<endl;
+		cout<<"init eXosip error"<<endl;
+		LOG(INFO)<<"init eXosip Error";
 	}
+	LOG(INFO)<<"Start StreamServer";
 	timespec time1;
 	clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time1);
-	
+	google::ShutdownGoogleLogging();
 	return 0;
 }
